844caa6642b152f7b7b14584e91a3be511b35773
std::move() <102883015+glibcxx@users.noreply.github.com>
2024-10-04 13:28:57 +0800

    now u can install through lip

--- xmake.lua
+++ xmake.lua
@@ -11,7 +11,7 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-target("figure-hack") -- Change this to your mod name.
+target("figure_hack") -- Change this to your mod name.
     add_cxflags(
         "/EHa",
         "/utf-8",
